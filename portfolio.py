0a73ee53504e027249187a6491bd66741487d179
Mauro Alves <mauro.mn@hotmail.com>
2023-06-30 01:06:37 -0300

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+
+# Título do portfólio
+st.title("Portfólio de Mauro Alves")
+
+# Informações de contato
+st.write("Entre em contato:")
+st.write("📧: seuemail@gmail.com")
+
+# Sobre você
+st.header("Sobre mim")
+st.write("""
+Sou um desenvolvedor Python apaixonado por aprendizado de máquina e ciência de dados.
+Tenho experiência em várias bibliotecas e ferramentas, incluindo Streamlit, Pandas, 
+Scikit-Learn e TensorFlow.
+""")
+
+# Habilidades
+st.header("Habilidades")
+columns = st.columns(3)  
+columns[0].button("Python")  
+columns[1].button("Streamlit")  
+columns[2].button("Machine Learning")
+
+# Educação
+st.header("Educação")
+df = pd.DataFrame({
+    'Instituição': ['Universidade Paulista'],
+    'Grau': ['Superior Completo'],
+    'Anos': ['2020 - 2023']
+})
+st.table(df)
+
+# Projetos
+st.header("Projetos")
+with st.expander('Video Converter Online'):
+    st.write("Descrição do projeto")
+
+with st.expander('search Jobs'):
+    st.write("Descrição do projeto")
+
+# Realizações
+st.header("Realizações")
+with st.expander('Nome da Realização'):
+    st.write("Descrição da realização")
