6bb28404122d938a3147acbc7ac5106af7cb2a86
Mauro Alves <mauro.mn@hotmail.com>
2023-07-03 13:52:38 -0300

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -7,13 +7,13 @@ st.divider()
 st.image("imagem/IMG_20230703_120107.png")
 
 st.markdown("""
-    <a href="https://github.com/Maurynn" target=" _blank" style="margin-right: 15px">
+    <a href="https://github.com/Maurynn" target="blank" style="margin-right: 15px">
         <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="Github logo" width="15" height="15">
     </a>
-    <a href="https://linkedin.com/in/maurosp" target=" _blank" style="margin-right: 15px">
+    <a href="https://linkedin.com/in/maurosp" target="_blank" style="margin-right: 15px">
         <img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="LinkedIn logo" width="15" height="15">
     </a>
-    <a href="https://instagram.com/maurinn?igshid=ZDc4ODBmNjlmNQ==" target=" _blank" style="margin-right: 15px">
+    <a href="https://instagram.com/maurinn?igshid=ZDc4ODBmNjlmNQ==" target="_blank" style="margin-right: 15px">
         <img src="https://instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico" alt="Instagram logo" width="15" height="15">
     </a>
 """, unsafe_allow_html=True)
